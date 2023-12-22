6f378d535001b3744f7d728d96f0795bee3792cc
sadohu <sadohu26@yahoo.com>
2023-12-22 16:23:18 -0500

    Add Company model and Company service

---
+++ src/app/models/company.model.ts
@@ -0,0 +1,10 @@
+export class Company {
+    idCompany?: number;
+    businessName?: string;
+    idNumber?: string;
+    address?: string;
+    tradeName?: string;
+    phone?: string;
+    // createdAt?: Date;
+    // updatedAt?: Date;
+}
