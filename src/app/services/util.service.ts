2a31fff6d6ed4305ab30145be26b26090609ba54
sadohu <sadohu26@yahoo.com>
2023-12-24 01:42:38 -0500

    Add UtilService for API integration

---
+++ src/app/services/util.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { AppSettings } from '../app.settings';
+import { Observable } from 'rxjs';
+
+const urlRUC = AppSettings.API_APIPERU_RUC;
+const urlDNI = AppSettings.API_APIPERU_DNI;
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImxyMjcwNzIwMDFAZ21haWwuY29tIn0.uSeFR7UMIxYmAzZmllUAVPBZHFiqghwmoMYoFTbhTIo';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UtilService {
+
+  constructor(private http: HttpClient) { }
+
+  searchRUC(ruc: string): Observable<any> {
+    const params = new HttpParams().set("token", token);
+    return this.http.get<any>(urlRUC + '/' + ruc, { params });
+  }
+}
