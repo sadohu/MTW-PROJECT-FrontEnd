d3acdaa01f001054bee90baa3317c1c4c37027e1
sadohu <sadohu26@yahoo.com>
2023-12-29 00:25:10 -0500

    Add menu and table with drivers information

--- src/app/components/driver-main/driver-main.component.ts
+++ src/app/components/driver-main/driver-main.component.ts
@@ -1,4 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { SwalCustoms } from 'src/app/Utils/SwalCustoms';
+import { CompanySaveComponent } from 'src/app/dialogs/company-save/company-save.component';
+import { Company } from 'src/app/models/company.model';
+import { Driver } from 'src/app/models/driver.model';
+import { DriverService } from 'src/app/services/driver.service';
 
 @Component({
   selector: 'app-driver-main',
@@ -7,5 +16,47 @@ import { Component, ViewChild } from '@angular/core';
 })
 
 export class DriverMainComponent {
+  filter: string = "";
+  dataSource: any;
 
+  displayedColumns = ["names", "lastNames", "idNumber", "phone", "brand", "model", "carPlate", "year", "color", "actions"];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator;
+
+  constructor(private driverService: DriverService, private dialogService: MatDialog, private router: Router) {
+    this.driverService.getAll().subscribe({
+      next: (response: Driver[]) => {
+        this.dataSource = new MatTableDataSource<Driver>(response);
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (error: any) => {
+        SwalCustoms.nyanAlert(error.message);
+      }
+    });
+  }
+
+  private refreshTable() {
+
+  }
+
+  search() {
+
+  }
+
+  newBooking(item: Driver) {
+
+  }
+
+  openAddDialog() {
+
+  }
+
+  openUpdateDialog(item: any) {
+
+  }
+
+  deleteRevista(item: any) {
+
+  }
 }
