e1f7b77b311b478e54e6939bd1c64343fef68202
sadohu <sadohu26@yahoo.com>
2024-01-02 16:11:50 -0500

    Update API endpoints in AppSettings

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,12 +1,20 @@
 export class AppSettings {
+    /* API endpoints */
     public static API_ENDPOINT = 'http://localhost:9090';
+
+    /* Auth endpoints */
     public static AUTH_SERVICE = AppSettings.API_ENDPOINT + '/auth';
+
+    /* Utils endpoints */
+    public static AREA_SERVICE = AppSettings.API_ENDPOINT + '/areas';
+    public static CURRENCY_SERVICE = AppSettings.API_ENDPOINT + '/currency';
+    public static UBIGEO_SERVICE = AppSettings.API_ENDPOINT + '/ubigeo';
+
+    /* Main endpoints */
     public static COMPANY_SERVICE = AppSettings.API_ENDPOINT + '/company';
     public static DRIVER_SERVICE = AppSettings.API_ENDPOINT + '/driver';
-    public static UBIGEO_SERVICE = AppSettings.API_ENDPOINT + '/ubigeo';
-    public static CURRENCY_SERVICE = AppSettings.API_ENDPOINT + '/currency';
-    public static AREA_SERVICE = AppSettings.API_ENDPOINT + '/areas';
 
+    /* API Apiperu */
     public static API_APIPERU = 'https://dniruc.apisperu.com/api/v1';
     public static API_APIPERU_RUC = AppSettings.API_APIPERU + '/ruc';
     public static API_APIPERU_DNI = AppSettings.API_APIPERU + '/dni';
