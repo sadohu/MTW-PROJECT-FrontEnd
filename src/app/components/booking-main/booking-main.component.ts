8f237f1452a38eb378d6adfd6ed8b1c0288cad1f
sadohu <sadohu26@yahoo.com>
2024-01-03 22:58:37 -0500

    Update booking-main.component.html and booking-main.component.ts

--- src/app/components/booking-main/booking-main.component.ts
+++ src/app/components/booking-main/booking-main.component.ts
@@ -4,6 +4,8 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Booking } from 'src/app/models/booking.model';
 import { BookingService } from 'src/app/services/booking.service';
+import { setListBookingToDto } from 'src/app/Utils/ModelsDto';
+import { BookingDto } from 'src/app/Utils/modelsDto/booking-dto.model';
 
 @Component({
   selector: 'app-booking-main',
@@ -16,7 +18,7 @@ export class BookingMainComponent {
   dataSource: any;
   newDriver: any = { idDriver: 0 };
 
-  displayedColumns = ["idBooking", "date", "time", "company", "applicant", "area", "passenger", "pickUp", "destination", "price", "status", "actions"];
+  displayedColumns = ["idBooking", "date", "time", "company", "applicant", "area", "passenger", "pickUp", "destination", "price", "driver", "status", "actions"];
   @ViewChild(MatPaginator, { static: true })
   paginator!: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort!: MatSort;
@@ -25,9 +27,9 @@ export class BookingMainComponent {
     this.refreshTable();
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyFilter() {
+    // const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = this.filter.trim().toLowerCase();
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
@@ -38,8 +40,9 @@ export class BookingMainComponent {
     this.bookingService.getAll().subscribe({
       next: (response: any) => {
         console.log("response", response);
-
-        this.dataSource = new MatTableDataSource<Booking>(response);
+        const listDto = setListBookingToDto(response);
+        console.log("listDto", listDto);
+        this.dataSource = new MatTableDataSource<BookingDto>(listDto);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
       },
