f9f2f61ef7b691f3fd2d1140b34d90537e918f34
sadohu <sadohu26@yahoo.com>
2024-01-03 22:58:33 -0500

    Add BookingDto model and setListBookingToDto function

--- src/app/Utils/ModelsDto.ts
+++ src/app/Utils/ModelsDto.ts
@@ -1,3 +1,6 @@
+import { Booking } from "../models/booking.model";
+import { BookingDto } from "./modelsDto/booking-dto.model";
+
 export class ClientDocument {
     idDocumentType?: number;
     description?: string;
@@ -8,3 +11,29 @@ export const listClientDocument: ClientDocument[] = [
     { idDocumentType: 1, description: 'DNI', apiDescription: 'dni' },
     { idDocumentType: 2, description: 'RUC', apiDescription: 'ruc' },
 ]
+
+export const setListBookingToDto = (list: Booking[]) => {
+    const listDto: BookingDto[] = [];
+    list.forEach(item => {
+        const dto = new BookingDto();
+        dto.idBooking = item.idBooking;
+        dto.date = item.date;
+        dto.time = item.time;
+        dto.company = item.company?.businessName;
+        dto.applicant = item.applicant;
+        dto.area = item.area?.name;
+        dto.passenger = item.passenger?.names + ' ' + item.passenger?.lastNames;
+        dto.pickUp = item.pickUp;
+        dto.ubigeoPickUp = item.ubigeoPickUp?.name;
+        dto.destination = item.destination;
+        dto.ubigeoDestination = item.ubigeoDestination?.name;
+        dto.notes = item.notes;
+        dto.currency = item.currency?.symbol;
+        dto.price = item.price;
+        dto.driver = item.driver?.names + ' ' + item.driver?.lastNames;
+        dto.status = item.status;
+        dto.bill = item.bill?.number;
+        listDto.push(dto);
+    });
+    return listDto;
+}
---
+++ src/app/Utils/modelsDto/booking-dto.model.ts
@@ -0,0 +1,19 @@
+export class BookingDto {
+    idBooking?: number;
+    date?: string;
+    time?: string;
+    company?: string;
+    applicant?: string;
+    area?: string;
+    passenger?: string;
+    pickUp?: string;
+    ubigeoPickUp?: string;
+    destination?: string;
+    ubigeoDestination?: string;
+    notes?: string;
+    currency?: string;
+    price?: number;
+    driver?: string;
+    status?: string;
+    bill?: string;
+}
